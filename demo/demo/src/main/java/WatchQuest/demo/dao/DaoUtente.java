0566610397de4b696efbc86b8f4144e15c714eef
IemRob <riemmolo376@msn.com>
2025-03-24 15:02:39 +0100

    dao utente commit

---
+++ demo/demo/src/main/java/WatchQuest/demo/dao/DaoUtente.java
@@ -0,0 +1,23 @@
+package WatchQuest.demo.dao;
+
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import WatchQuest.demo.database.DatabaseMySql;
+import WatchQuest.demo.entity.GenericEntity;
+import lombok.Data;
+
+@Data
+@Service
+public abstract class DaoUtente implements IDao{
+    
+    private final DatabaseMySql databaseMySql;
+    private final ApplicationContext context;
+    private final DaoSerie daoSerie;
+    private final DaoFilm daoFilm;
+    
+
+
+}
